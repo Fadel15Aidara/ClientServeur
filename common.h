a522580bd1cc6a077aa1bba8934cd1e89252b42d
Fadel15Aidara <ebyaidara@gmail.com>
2024-12-29 18:14:14 +0000

    Create common.h

---
+++ common.h
@@ -0,0 +1,13 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define PORT 8080
+#define BUFFER_SIZE 255
+
+  typedef struct
+  {
+    int compteur;
+  }Donne;
+  void server();
+  void client();
+#endif // COMMON_H
